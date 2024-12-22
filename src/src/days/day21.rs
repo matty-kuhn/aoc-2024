83957ffabe3bc9301b8447e49ffc6adf53cf90d6
Matty Kuhn <matty.kuhn.1@gmail.com>
2024-12-21 18:35:17 -0600

    day21 pt 1, gets the wrong answer sometimes randomly lol

--- src/src/days/day21.rs
+++ src/src/days/day21.rs
@@ -1,3 +1,10 @@
+use itertools::Itertools;
+use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::ParallelIterator;
+use std::sync::atomic::AtomicUsize;
+use std::sync::Arc;
+use std::{collections::HashMap, sync::LazyLock};
+
 use super::Day;
 
 pub struct Day21 {
@@ -12,10 +19,429 @@ impl Day21 {
 
 impl Day for Day21 {
     fn part1(&self) -> String {
-        todo!()
+        self.input
+            .lines()
+            .fold(0, |acc, input| {
+                // get the first robot's traversal of keypad
+                let num_keypad_paths = get_sequence_nums(input.to_owned());
+                // println!("{num_keypad_paths:?}");
+
+                // get the path to do that one
+                let mut exp = num_keypad_paths.clone();
+                for _ in 0..2 {
+                    let min_len = Arc::new(AtomicUsize::new(usize::MAX));
+                    exp = exp
+                        .par_iter()
+                        .map(|path| {
+                            get_sequence_dirs(path.clone())
+                                .iter()
+                                .map(|p| (p.clone(), p.len()))
+                                .collect::<Vec<(String, usize)>>()
+                        })
+                        .flatten()
+                        .map(|(p, len)| {
+                            if len < min_len.load(std::sync::atomic::Ordering::SeqCst) {
+                                min_len.store(len, std::sync::atomic::Ordering::SeqCst);
+                            }
+                            (p, len)
+                        })
+                        .filter_map(|(p, len)| {
+                            if len == min_len.load(std::sync::atomic::Ordering::SeqCst) {
+                                Some(p)
+                            } else {
+                                None
+                            }
+                            // Some(p)
+                        })
+                        .collect();
+                }
+                acc + (score_code(input) * exp[0].len())
+            })
+            .to_string()
     }
 
     fn part2(&self) -> String {
         todo!()
     }
+}
+
+fn score_code(input: &str) -> usize {
+    // first 3 chars of input are number
+    input[..3].parse().unwrap()
+}
+
+#[memoize::memoize]
+fn get_sequence_nums(input: String) -> Vec<String> {
+    let mut res: Vec<String> = vec![String::new()];
+    let input = format!("A{}", input);
+    let mut curr = 0;
+    let mut target = 1;
+
+    loop {
+        if target == input.len() || curr == input.len() - 1 {
+            break;
+        }
+        let curr_ch = input.chars().nth(curr).unwrap();
+        let target_ch = input.chars().nth(target).unwrap();
+        let path = traverse_num_keypad(curr_ch, target_ch);
+        let mut temp = Vec::new();
+        for adds in path {
+            for r in &res {
+                temp.push(format!("{}{}", r, adds.iter().collect::<String>()));
+            }
+        }
+        res = temp;
+        curr = target;
+        target += 1;
+    }
+    res
+}
+
+#[memoize::memoize]
+fn get_sequence_dirs(input: String) -> Vec<String> {
+    let mut res: Vec<String> = vec![String::new()];
+    let input = format!("A{}", input);
+    let mut curr = 0;
+    let mut target = 1;
+
+    loop {
+        if target == input.len() || curr == input.len() - 1 {
+            break;
+        }
+        let curr_ch = input.chars().nth(curr).unwrap();
+        let target_ch = input.chars().nth(target).unwrap();
+        let path = traverse_dir_keypad(curr_ch, target_ch);
+        let mut temp = Vec::new();
+        for adds in path {
+            for r in &res {
+                temp.push(format!("{}{}", r, adds.iter().collect::<String>()));
+            }
+        }
+        res = temp;
+        curr = target;
+        target += 1;
+    }
+    res
+}
+
+#[memoize::memoize]
+fn traverse_dir_keypad(curr_pos: char, target: char) -> Vec<Vec<char>> {
+    static KEYPAD: [[char; 3]; 2] = [[' ', '^', 'A'], ['<', 'v', '>']];
+    static COORDS: LazyLock<HashMap<char, (usize, usize)>> = LazyLock::new(|| {
+        let mut map = HashMap::new();
+        for (i, row) in KEYPAD.iter().enumerate() {
+            for (j, c) in row.iter().enumerate() {
+                map.insert(*c, (j, i));
+            }
+        }
+        map
+    });
+    if curr_pos == target {
+        return vec![vec!['A']];
+    }
+    let curr_pos_coords = COORDS.get(&curr_pos).unwrap().clone();
+    let target_coords = COORDS.get(&target).unwrap();
+
+    let mut temp = Vec::new();
+    // if we need to move up, handle left/right first
+    // if we need to move down, do that first
+    if curr_pos_coords.1 > target_coords.1 {
+        if curr_pos_coords.0 > target_coords.0 {
+            // move left
+            for _ in 0..(curr_pos_coords.0 - target_coords.0) {
+                temp.push('<');
+            }
+        } else {
+            // move right or stay in place
+            for _ in 0..(target_coords.0 - curr_pos_coords.0) {
+                temp.push('>');
+            }
+        }
+        // finally move up
+        temp.push('^');
+    } else if curr_pos_coords.1 < target_coords.1 {
+        // move down
+        temp.push('v');
+        // handle left/right
+        if curr_pos_coords.0 > target_coords.0 {
+            // move left
+            for _ in 0..(curr_pos_coords.0 - target_coords.0) {
+                temp.push('<');
+            }
+        } else {
+            // move right or stay in place
+            for _ in 0..(target_coords.0 - curr_pos_coords.0) {
+                temp.push('>');
+            }
+        }
+    } else {
+        // just move left/right
+        if curr_pos_coords.0 > target_coords.0 {
+            // move left
+            for _ in 0..(curr_pos_coords.0 - target_coords.0) {
+                temp.push('<');
+            }
+        } else {
+            // move right or stay in place
+            for _ in 0..(target_coords.0 - curr_pos_coords.0) {
+                temp.push('>');
+            }
+        }
+    }
+    let mut res: Vec<Vec<char>> = temp
+        .clone()
+        .into_iter()
+        .permutations(temp.len())
+        .unique()
+        .filter(|walk| {
+            let mut curr_coords = curr_pos_coords.clone();
+            for dir in walk {
+                match dir {
+                    '^' => {
+                        curr_coords.1 -= 1;
+                        if curr_coords == *COORDS.get(&' ').unwrap() {
+                            return false;
+                        }
+                    }
+                    'v' => {
+                        curr_coords.1 += 1;
+                        if curr_coords == *COORDS.get(&' ').unwrap() {
+                            return false;
+                        }
+                    }
+                    '<' => {
+                        curr_coords.0 -= 1;
+                        if curr_coords == *COORDS.get(&' ').unwrap() {
+                            return false;
+                        }
+                    }
+                    '>' => {
+                        curr_coords.0 += 1;
+                        if curr_coords == *COORDS.get(&' ').unwrap() {
+                            return false;
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+
+            true
+        })
+        .collect();
+    for r in res.iter_mut() {
+        r.push('A');
+    }
+
+    res
+}
+
+#[memoize::memoize]
+fn traverse_num_keypad(curr_pos: char, target: char) -> Vec<Vec<char>> {
+    static KEYPAD: [[char; 3]; 4] = [
+        ['7', '8', '9'],
+        ['4', '5', '6'],
+        ['1', '2', '3'],
+        [' ', '0', 'A'],
+    ];
+    static COORDS: LazyLock<HashMap<char, (usize, usize)>> = LazyLock::new(|| {
+        let mut map = HashMap::new();
+        for (i, row) in KEYPAD.iter().enumerate() {
+            for (j, c) in row.iter().enumerate() {
+                map.insert(*c, (j, i));
+            }
+        }
+        map
+    });
+    // println!("{} {}", curr_pos, target);
+    if curr_pos == target {
+        return vec![vec!['A']];
+    }
+    let curr_pos_coords = COORDS.get(&curr_pos).unwrap().clone();
+    let target_coords = COORDS.get(&target).unwrap();
+
+    let mut temp = Vec::new();
+    // get a list of directions we need to move
+    if curr_pos_coords.1 < target_coords.1 {
+        if curr_pos_coords.0 > target_coords.0 {
+            // move left
+            for _ in 0..(curr_pos_coords.0 - target_coords.0) {
+                temp.push('<');
+            }
+        } else {
+            // move right or stay in place
+            for _ in 0..(target_coords.0 - curr_pos_coords.0) {
+                temp.push('>');
+            }
+        }
+        // finally move down
+        for _ in 0..(target_coords.1 - curr_pos_coords.1) {
+            temp.push('v');
+        }
+    } else if curr_pos_coords.1 > target_coords.1 {
+        // move up
+        for _ in 0..(curr_pos_coords.1 - target_coords.1) {
+            temp.push('^');
+        }
+        // handle left/right
+        if curr_pos_coords.0 > target_coords.0 {
+            // move left
+            for _ in 0..(curr_pos_coords.0 - target_coords.0) {
+                temp.push('<');
+            }
+        } else {
+            // move right or stay in place
+            for _ in 0..(target_coords.0 - curr_pos_coords.0) {
+                temp.push('>');
+            }
+        }
+    } else {
+        // just move left/right
+        if curr_pos_coords.0 > target_coords.0 {
+            // move left
+            for _ in 0..(curr_pos_coords.0 - target_coords.0) {
+                temp.push('<');
+            }
+        } else {
+            // move right or stay in place
+            for _ in 0..(target_coords.0 - curr_pos_coords.0) {
+                temp.push('>');
+            }
+        }
+    }
+
+    // get every possible ordering of directions
+    let mut res: Vec<Vec<char>> = temp
+        .clone()
+        .into_iter()
+        .permutations(temp.len())
+        .unique()
+        .filter(|walk| {
+            let mut curr_coords = curr_pos_coords.clone();
+            for dir in walk {
+                match dir {
+                    '^' => {
+                        curr_coords.1 -= 1;
+                        if curr_coords == *COORDS.get(&' ').unwrap() {
+                            return false;
+                        }
+                    }
+                    'v' => {
+                        curr_coords.1 += 1;
+                        if curr_coords == *COORDS.get(&' ').unwrap() {
+                            return false;
+                        }
+                    }
+                    '<' => {
+                        curr_coords.0 -= 1;
+                        if curr_coords == *COORDS.get(&' ').unwrap() {
+                            return false;
+                        }
+                    }
+                    '>' => {
+                        curr_coords.0 += 1;
+                        if curr_coords == *COORDS.get(&' ').unwrap() {
+                            return false;
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+
+            true
+        })
+        .collect();
+    for r in res.iter_mut() {
+        r.push('A');
+    }
+
+    res
+}
+
+fn walk_backwards_nums(input: &str) -> String {
+    static KEYPAD: [[char; 3]; 4] = [
+        ['7', '8', '9'],
+        ['4', '5', '6'],
+        ['1', '2', '3'],
+        [' ', '0', 'A'],
+    ];
+    static COORDS: LazyLock<HashMap<char, (usize, usize)>> = LazyLock::new(|| {
+        let mut map = HashMap::new();
+        for (i, row) in KEYPAD.iter().enumerate() {
+            for (j, c) in row.iter().enumerate() {
+                map.insert(*c, (j, i));
+            }
+        }
+        map
+    });
+
+    let mut start_coords = COORDS.get(&'A').unwrap().clone();
+
+    let mut res = String::new();
+    for ch in input.chars() {
+        match ch {
+            '^' => {
+                start_coords.1 -= 1;
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+            }
+            'v' => {
+                start_coords.1 += 1;
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+            }
+            '<' => {
+                start_coords.0 -= 1;
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+            }
+            '>' => {
+                start_coords.0 += 1;
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+            }
+            'A' => {
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+                res.push(KEYPAD[start_coords.1][start_coords.0]);
+            }
+            _ => unreachable!(),
+        }
+    }
+    res
+}
+
+fn walk_backwards_dirs(input: &str) -> String {
+    static KEYPAD: [[char; 3]; 2] = [[' ', '^', 'A'], ['<', 'v', '>']];
+    static COORDS: LazyLock<HashMap<char, (usize, usize)>> = LazyLock::new(|| {
+        let mut map = HashMap::new();
+        for (i, row) in KEYPAD.iter().enumerate() {
+            for (j, c) in row.iter().enumerate() {
+                map.insert(*c, (j, i));
+            }
+        }
+        map
+    });
+    let mut start_coords = COORDS.get(&'A').unwrap().clone();
+    let mut res = String::new();
+    for ch in input.chars() {
+        match ch {
+            '^' => {
+                start_coords.1 -= 1;
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+            }
+            'v' => {
+                start_coords.1 += 1;
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+            }
+            '<' => {
+                start_coords.0 -= 1;
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+            }
+            '>' => {
+                start_coords.0 += 1;
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+            }
+            'A' => {
+                assert!(start_coords != *COORDS.get(&' ').unwrap());
+                res.push(KEYPAD[start_coords.1][start_coords.0]);
+            }
+            _ => unreachable!(),
+        }
+    }
+    res
 }
