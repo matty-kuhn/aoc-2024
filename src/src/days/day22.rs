6d1f39f415a994e2d1179d9e9c75cb19b738db0c
Matty Kuhn <matty.kuhn.1@gmail.com>
2024-12-21 23:55:08 -0600

    day22 pt 1

--- src/src/days/day22.rs
+++ src/src/days/day22.rs
@@ -12,7 +12,25 @@ impl Day22 {
 
 impl Day for Day22 {
     fn part1(&self) -> String {
-        todo!()
+        // step 1: secret = ((secret << 6) ^ secret) % 16777216
+        // step 2: secret = ((secret >> 5) ^ secret) % 16777216
+        // step 3: secret = ((secret  << 11) ^ secret) % 16777216
+        self.input
+            .lines()
+            .fold(0, |acc, num| {
+                let mut secret = num.parse::<usize>().unwrap();
+                for _ in 0..2000 {
+                    secret = ((secret << 6) ^ secret) % 16777216;
+                    secret = ((secret >> 5) ^ secret) % 16777216;
+                    secret = ((secret << 11) ^ secret) % 16777216;
+                }
+                // dbg!(secret);
+
+                acc + secret
+            })
+            .to_string()
+
+        // todo!()
     }
 
     fn part2(&self) -> String {
